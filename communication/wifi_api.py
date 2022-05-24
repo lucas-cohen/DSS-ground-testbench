7c17ecf0602822a6f5316bb1d82eb5c0512beaaa
Lucas Cohen <LucascohenMail@gmail.com>
2022-05-24 14:50:07 +0200

    two usb serial com int

--- communication/wifi_api.py
+++ communication/wifi_api.py
@@ -1,15 +1,53 @@
-# Importing Libraries
+### Importing Libraries ###
+
+# Importing Arduino serial comunication
 import serial
+import serial.tools.list_ports
+
 import time
 
 
-arduino = serial.Serial(port='COM4', baudrate=115200, timeout=.1)
-def write_read(x):
+
+def get_ports(inc_lics=False):
+    """Get availabe serial ports in list of PortInfo object.
+    Args:
+        inc_lics (bool, optional): include symlinks. Defaults to False.
+    """
+    
+    ports = serial.tools.list_ports.comports(inc_lics)
+    return ports
+
+
+def select_ports(ports, selected_idx):
+    pass
+
+
+def write_read_both(x):
     arduino.write(bytes(x, 'utf-8'))
+    robot.write(bytes(x, 'utf-8'))
     time.sleep(0.05)
-    data = arduino.readline()
-    return data
-while True:
-    num = input("Enter a number: ") # Taking input from user
-    value = write_read(num)
-    print(value) # printing the value
+    data_arduino = arduino.readline()
+    data_robot = robot.readline()
+    return data_arduino, data_robot
+
+def wifi_write_read():
+    pass
+
+def main():
+    #arduino = serial.Serial(port='/dev/cu.usbmodem14601', baudrate=115200, timeout=.1)
+    robot = serial.Serial(port='/dev/cu.usbserial-A506NFKT', baudrate=115200, timeout=.1)
+    
+    # while True:
+    #     # num = input("Enter a number: ") # Taking input from user
+    #     # value_arduino, value_robot = write_read_both(num)
+        
+    #     # print(f"{value_arduino = }, \t {value_robot = }")
+        
+        
+    for port in get_ports():
+        print(port.device)
+
+if __name__ == "__main__":
+    main()
+
+
